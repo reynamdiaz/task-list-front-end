d9a6a9ab2fe19910bff0e2c00393d3cc1a31a125
Reyna Diaz <reynamdiazz@gmail.com>
2022-12-14 15:05:06 -0500

    "wave 01 setup and baseline"

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import Task from './Task';
 import './TaskList.css';
 
-const TaskList = ({ tasks }) => {
-  const getTaskListJSX = (tasks) => {
-    return tasks.map((task) => {
+const TaskList = (props) => {
+  // const TaskList = (tasks) => {
+  const getTaskListJSX = (props) => {
+    return props.tasks.map((task) => {
+      // return tasks.tasks.map((task) => {
       return (
         <Task
           key={task.id}
@@ -16,7 +18,8 @@ const TaskList = ({ tasks }) => {
       );
     });
   };
-  return <ul className="tasks__list no-bullet">{getTaskListJSX(tasks)}</ul>;
+  return <ul className="tasks__list no-bullet">{getTaskListJSX(props)}</ul>;
+  // return <ul className="tasks__list no-bullet">{getTaskListJSX(tasks)}</ul>;
 };
 
 TaskList.propTypes = {
