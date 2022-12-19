c2cf019fca7c788c6d1d2c83c991345544eadeb5
Reyna Diaz <reynamdiazz@gmail.com>
2022-12-19 17:20:00 -0500

    "comments/plans for wave03"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useState } from 'react';
+// import { useEffect } from 'react';
+// import axios from 'axios';
 import TaskList from './components/TaskList.js';
 import './App.css';
 
+// maybe delete this? (receiving tasks from API) lines 9 - 20
 const TASKSLIST = [
   {
     id: 1,
@@ -17,11 +20,17 @@ const TASKSLIST = [
 ];
 
 const App = () => {
+  // useEffect to grab list of tasks from DB to use as state
+  // useEffect(()=> {1st param, 2nd param})
+  // replace state with list of tasks from useEffect
   const [tasks, setTasks] = useState(TASKSLIST);
   // const [tasks, setComplete] = useState(TASKSLIST);
   console.log('tasklist:', tasks);
 
   console.log(tasks);
+  // update completeTask function (toggleCompleteTask in README)
+  // to update task isComplete in the DB
+  // figure out how
   const completeTask = (id) => {
     setTasks((tasks) =>
       tasks.map((task) => {
@@ -36,15 +45,15 @@ const App = () => {
     );
   };
 
-// update tasks, leverage the state
-  const deleteTask = id => {
+  // update tasks, leverage the state
+  const deleteTask = (id) => {
     console.log('in delete!');
     console.log('deletable task');
-    // fix functionality! wrap next three lines in 
-    let updatedTasks = tasks.filter(task => task.id !== id);
+    // fix functionality! wrap next three lines in
+    let updatedTasks = tasks.filter((task) => task.id !== id);
     console.log(updatedTasks);
-    setTasks(updatedTasks); 
-    };
+    setTasks(updatedTasks);
+  };
 
   return (
     <div className="App">
@@ -52,11 +61,15 @@ const App = () => {
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList 
-                  tasks={tasks} 
-                  onCompleteTask={completeTask} 
-                  onDeleteTask={deleteTask}
-                  />}</div>
+        <div>
+          {
+            <TaskList
+              tasks={tasks}
+              onCompleteTask={completeTask}
+              onDeleteTask={deleteTask}
+            />
+          }
+        </div>
       </main>
     </div>
   );
